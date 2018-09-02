ced2bfc9da1ed0971a4fd4b07c92ab1cb517a597
Paweł Bulwan <git@taurit.pl>
2018-09-02 18:47:06 +0200

    Add tests for number of minutes defined in a natural language for en culture
    The test currently fails.

--- NaturalLanguageTimespanParser/NaturalLanguageTimespanParser.UnitTests/CultureSpecificParsersTests/TimespanParserTestsEn.cs
+++ NaturalLanguageTimespanParser/NaturalLanguageTimespanParser.UnitTests/CultureSpecificParsersTests/TimespanParserTestsEn.cs
@@ -26,6 +26,35 @@ namespace NaturalLanguageTimespanParser.UnitTests.CultureSpecificParsersTests
             // Assert
             AssertAllDurationsEqual(TimeSpan.FromMinutes(45), results45Min);
         }
+        
+        [TestMethod]
+        public override void When_TimeInMinutesIsDefinedInWords_Expect_DurationCorrectlyParsed()
+        {
+            // Arrange
+            var sut = CreateSystemUnderTest();
+            List<string> sentences = new List<string>()
+            {
+                "@home Read for zero minutes",
+                "@home Read for one minute",
+                "@home Read for two minutes",
+                "@home Read for three minutes",
+                "@home Read for four minutes",
+                "@home Read for five minutes",
+                "@home Read for six minutes",
+                "@home Read for seven minutes",
+                "@home Read for eight minutes",
+                "@home Read for nine minutes",
+            };
+
+            // Act
+            var results = sentences.Select(sut.Parse).ToList();
+            
+            // Assert
+            for (int i = 0; i < sentences.Count; i++)
+            {       
+                Assert.Equals(TimeSpan.FromMinutes(i), results[i]);
+            }
+        }
 
         internal override ITimespanParser CreateSystemUnderTest()
         {
